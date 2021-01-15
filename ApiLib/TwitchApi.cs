2c7ad6dd606141f9c3d0e13e9ef088a1559f355f
jose_x_744 <jose_x_744@hotmail.com>
2021-01-15 19:40:57 +0100

    Agregar archivos de proyecto.

---
+++ ApiLib/TwitchApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchJ.ApiLib
+{
+    //Didn´t use since you need a OAuth/Server 
+    public class TwitchApi : IApiFactory
+    {
+        private static TwitchApi _Instance;
+
+        public static TwitchApi GetInstance()
+        {
+            if(_Instance == null)
+            {
+                _Instance = new TwitchApi();
+            }
+            return _Instance;
+        }
+
+        public string Name { get; set; } = "Twitch";
+        public string Key { get; set; } = "";
+
+        public bool Connection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
